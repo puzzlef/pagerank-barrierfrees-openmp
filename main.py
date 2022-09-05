b07b635db076b8a9643c686d35434e6a5762e85c
Subhajit Sahu <wolfram77@gmail.com>
2022-09-05 13:08:22 +0530

    :mango: w/ expt

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# https://www.kaggle.com/wolfram77/puzzlef-pagerank-barrierfree-openmp-adjust-tolerance
+# https://www.kaggle.com/wolfram77/puzzlef-pagerank-barrierfree-openmp-fixed-iterations
 import os
 from IPython.display import FileLink
-src="pagerank-barrierfree-openmp-adjust-tolerance"
+src="pagerank-barrierfree-openmp-fixed-iterations"
 inp="/kaggle/input/graphs"
 out="{}.txt".format(src)
 !printf "" > "$out"
