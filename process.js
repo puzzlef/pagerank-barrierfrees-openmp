594962362eedc324da2fc5d83193831513e62a0e
Subhajit Sahu <wolfram77@gmail.com>
2022-11-22 16:24:11 +0530

    :bug: process output logs

--- process.js
+++ process.js
@@ -4,7 +4,9 @@ const path = require('path');
 
 const RGRAPH = /^Loading graph .*\/(.+?)\.mtx \.\.\./m;
 const RORDER = /^order: (\d+) size: (\d+) \[directed\] \{\} \(transposeWithDegree\)$/m;
-const RTHRDS = /^OMP_NUM_THREADS=(\d+)/m;
+const ROMPTH = /^OMP_NUM_THREADS=(\d+)/m;
+const RLOOPF = /^pagerank\w+LoopU\(\):/m;
+const RTSTAT = /^\[thread (\d+)\] status {processed=(\d+), stolen=(\d+), slept=(\d+), blocked=(\S+) ms}/m;
 const RRESLT = /^\[(.+?) ms; (.+?) iters\.\] \[(.+?) err\.\] (\w+)(?:\s+\{sleep_prob: (.+?), sleep_dur: (\d+) ms\})?/m;
 
 
@@ -54,20 +56,52 @@ function readLogLine(ln, data, state) {
     state.order = parseFloat(order);
     state.size  = parseFloat(size);
   }
-  else if (RTHRDS.test(ln)) {
-    var [, omp_num_threads] = RTHRDS.exec(ln);
+  else if (ROMPTH.test(ln)) {
+    var [, omp_num_threads] = ROMPTH.exec(ln);
     state.omp_num_threads   = parseFloat(omp_num_threads);
+    state.repeat            = 0;
+  }
+  else if (RLOOPF.test(ln)) {
+    state.repeat++;
+    state.time       = 0;
+    state.iterations = 0;
+    state.error      = 0;
+    state.technique  = '';
+    state.sleep_probability = 0;
+    state.sleep_duration    = 0;
+  }
+  else if (RTSTAT.test(ln)) {
+    var [, thread, processed_count, stolen_count, slept_count, blocked_time] = RTSTAT.exec(ln);
+    data.get(state.graph).push(Object.assign({}, state, {
+      thread: parseFloat(thread),
+      processed_count: parseFloat(processed_count),
+      stolen_count:    parseFloat(stolen_count),
+      slept_count:     parseFloat(slept_count),
+      blocked_time:    parseFloat(blocked_time),
+    }));
   }
   else if (RRESLT.test(ln)) {
     var [, time, iterations, error, technique, sleep_probability, sleep_duration] = RRESLT.exec(ln);
-    data.get(state.graph).push(Object.assign({}, state, {
-      time:              parseFloat(time),
-      iterations:        parseFloat(iterations),
-      error:             parseFloat(error),
+    var result = {
+      time:       parseFloat(time),
+      iterations: parseFloat(iterations),
+      error:      parseFloat(error),
       technique,
       sleep_probability: parseFloat(sleep_probability || '0'),
       sleep_duration:    parseFloat(sleep_duration    || '0'),
-    }));
+    };
+    var xdata = data.get(state.graph);
+    for (var i=xdata.length-1; i>=0; --i) {
+      if (xdata[i].technique) break;
+      Object.assign(xdata[i], result);
+    }
+    state.repeat = 0;
+    state.thread = 0;
+    state.processed_count = 0;
+    state.stolen_count    = 0;
+    state.slept_count     = 0;
+    state.blocked_time    = 0;
+    data.get(state.graph).push(Object.assign({}, state, result));
   }
   return state;
 }
